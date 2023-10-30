3e5bd7bb600bb1e58a9ffcfd2f6df2cfe27875a0
theodoremoreland <theodoremoreland@yahoo.com>
2023-10-30 00:53:02 -0500

    added jsdoc comments

--- scripts/grabAndMask.js
+++ scripts/grabAndMask.js
@@ -5,33 +5,69 @@
  */ 
 
 
+/**
+ * Immediately invoked function expression (IIFE) to prevent global scope pollution when executing directly in browser dev console.
+ */
 (() =>  {
     let intervalId = undefined;
 
+    /** Grab each order DOM element.
+     * @returns {Array} An array of DOM elements, each representing a single order.
+     */
     const grabOrderElements = () => {
         return [...document.querySelectorAll(`[data-anchor-id="OrderHistoryOrderItem"]`)];
     }
 
+    /**
+     * Returns the DOM element containing the name of the order.
+     * @param {Element} orderElement 
+     * @returns {Element} The DOM element containing the name of the order.
+     */
     const grabNameElement = (orderElement) => {
         return orderElement.querySelector(".iKPPDs");
     }
 
+    /**
+     * Returns the DOM element containing the date of the order.
+     * @param {Element} orderElement 
+     * @returns {Element} The DOM element containing the date of the order.
+     */
     const grabDateElement = (orderElement) => {
         return orderElement.querySelector(`.jPYUXx[color="TextTertiary"]`);
     }
 
+    /**
+     * Returns the DOM element containing the cost of the order.
+     * @param {Element} orderElement 
+     * @returns {Element} The DOM element containing the cost of the order.
+     */
     const grabCostElement = (orderElement) => {
         return orderElement.querySelector(`.jPYUXx[color="TextTertiary"]`);
     }
 
+    /**
+     * Returns the DOM element containing the item count of the order.
+     * @param {Element} orderElement 
+     * @returns {Element} The DOM element containing the item count of the order.
+     */
     const grabItemCountElement = (orderElement) => {
         return orderElement.querySelector(`.jPYUXx[color="TextTertiary"]`);
     }
 
+    /**
+     * Return the DOM element containing the items of the order.
+     * @param {Element} orderElement 
+     * @returns {Element} The DOM element containing the items of the order.
+     */
     const grabItemsElement = (orderElement) => {
         return orderElement.querySelector(".buMDwO");
     }
 
+    /**
+     * Checks to see if order was cancelled.
+     * @param {Element} orderElement 
+     * @returns {Boolean} True if order was cancelled, false otherwise.
+     */
     const checkWasCancelled = (orderElement) => {
         if (orderElement.querySelector(".gQhqKn") !== null) {
             return true;
@@ -40,10 +76,17 @@
         return false;
     }
 
+    /**
+     * Masks the name of the order.
+     */
     const maskName = (nameElement) => {
         nameElement.innerText = "Cat Express";
     }
 
+    /**
+     * Masks the date of the order.
+     * @param {Element} dateElement 
+     */
     const maskDate = (dateElement) => {
         const elementTextArray = dateElement
             .innerText
@@ -54,6 +97,10 @@
         dateElement.innerText = elementTextArray.join(" • ");
     }
 
+    /**
+     * Masks the cost of the order.
+     * @param {Element} costElement 
+     */
     const maskCost = (costElement) => {
         const elementTextArray = costElement
             .innerText
@@ -64,6 +111,10 @@
         costElement.innerText = elementTextArray.join(" • ");
     }
 
+    /**
+     * Masks the item count of the order.
+     * @param {Element} itemCountElement 
+     */
     const maskItemCount = (itemCountElement) => {
         const elementTextArray = itemCountElement
             .innerText
@@ -74,10 +125,18 @@
         itemCountElement.innerText = elementTextArray.join(" • ");
     }
 
+    /**
+     * Masks the items of the order.
+     * @param {Element} itemsElement 
+     */
     const maskItems = (itemsElement) => {
         itemsElement.innerText = "Mac & Cheese • Cheesecake  • Salmon  • Broccoli";
     }
 
+    /**
+     * Masks the order.
+     * @param {Element} orderElement 
+     */
     const maskOrder = (orderElement) => {
         const nameElement = grabNameElement(orderElement);
         const dateElement = grabDateElement(orderElement);
