054b6ab0f68dbd9b2d4aec735eff018bc7e78e10
theodoremoreland <theodoremoreland@yahoo.com>
2024-05-03 21:09:06 -0500

    added first two unit tests

--- app/src/__tests__/Analytics.test.ts
+++ app/src/__tests__/Analytics.test.ts
@@ -1,1 +1,17 @@
+import { expect, test, describe } from 'vitest'
+
+
 import Analytics from "../classes/Analytics";
+import mockOrders from './mocks/mockOrders';
+
+describe("Analytics", () => {
+  new Analytics(mockOrders);
+
+  test("should get accurate count of total purchases", () => {
+    expect(Analytics.getTotalPurchases()).toEqual(mockOrders.length);
+  });
+
+  test("should get accurate total spent", () => {
+    expect(Analytics.getTotalSpent()).toEqual(450);
+  });
+});
