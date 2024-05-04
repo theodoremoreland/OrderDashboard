fa70a6e17123d81be7d60c2dd871e14ab87409b8
theodoremoreland <theodoremoreland@yahoo.com>
2024-05-03 21:08:50 -0500

    added mock orders

---
+++ app/src/__tests__/mocks/mockOrders.ts
@@ -0,0 +1,88 @@
+import { Order } from "../../classes/Analytics";
+
+
+const mockOrders: Order[] = [
+    {
+        storeName: "Walmart",
+        date: "Jan 13 2020",
+        dayOfWeek: "Fri",
+        cost: 50,
+        items: ["apple", "banana", "orange"],
+        wasCancelled: false,
+        itemCount: 3,
+    },
+    {
+        storeName: "Walgreens",
+        date: "Jan 15 2020",
+        dayOfWeek: "Fri",
+        cost: 50,
+        items: ["apple", "banana", "orange"],
+        wasCancelled: false,
+        itemCount: 3,
+    },
+    {
+        storeName: "Aldi",
+        date: "Jan 20 2020",
+        dayOfWeek: "Fri",
+        cost: 50,
+        items: ["apple", "banana", "orange"],
+        wasCancelled: false,
+        itemCount: 3,
+    },
+    {
+        storeName: "Dierbergs",
+        date: "Jan 21 2020",
+        dayOfWeek: "Fri",
+        cost: 50,
+        items: ["apple", "banana", "orange"],
+        wasCancelled: false,
+        itemCount: 3,
+    },
+    {
+        storeName: "Applebees",
+        date: "Jan 21 2020",
+        dayOfWeek: "Fri",
+        cost: 50,
+        items: ["apple", "banana", "orange"],
+        wasCancelled: false,
+        itemCount: 3,
+    },
+    {
+        storeName: "Five Guys",
+        date: "Jan 23 2020",
+        dayOfWeek: "Fri",
+        cost: 50,
+        items: ["apple", "banana", "orange"],
+        wasCancelled: false,
+        itemCount: 3,
+    },
+    {
+        storeName: "McDonalds",
+        date: "Jan 24 2020",
+        dayOfWeek: "Fri",
+        cost: 50,
+        items: ["apple", "banana", "orange"],
+        wasCancelled: false,
+        itemCount: 3,
+    },
+    {
+        storeName: "Burger King",
+        date: "Jan 25 2020",
+        dayOfWeek: "Fri",
+        cost: 50,
+        items: ["apple", "banana", "orange"],
+        wasCancelled: false,
+        itemCount: 3,
+    },
+    {
+        storeName: "Taco Bell",
+        date: "Jan 26 2020",
+        dayOfWeek: "Fri",
+        cost: 50,
+        items: ["apple", "banana", "orange"],
+        wasCancelled: false,
+        itemCount: 3,
+    }
+];
+
+export default mockOrders;
