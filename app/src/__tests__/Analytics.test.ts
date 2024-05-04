43d789e6a6af95453df62d17b6200380e02ab4d2
theodoremoreland <theodoremoreland@yahoo.com>
2024-05-03 23:17:48 -0500

    added a couple more unit tests

--- app/src/__tests__/Analytics.test.ts
+++ app/src/__tests__/Analytics.test.ts
@@ -14,4 +14,12 @@ describe("Analytics", () => {
   test("should get accurate total spent", () => {
     expect(Analytics.getTotalSpent()).toEqual(450);
   });
+
+  test("should get accurate number of total items purchased", () => {
+    expect(Analytics.getTotalItemsPurchased()).toEqual(27);
+  });
+
+  test("should get accurate number of stores purchased from", () => {
+    expect(Analytics.getNumberOfStoresPurchasedFrom()).toEqual(9);
+  });
 });
