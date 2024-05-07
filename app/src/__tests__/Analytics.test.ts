cac94a636ad342cec9fd22bbc42ff14f4e9dc4c0
theodoremoreland <theodoremoreland@yahoo.com>
2024-05-06 20:04:31 -0500

    added unit tests for aggs of certain group bys

--- app/src/__tests__/Analytics.test.ts
+++ app/src/__tests__/Analytics.test.ts
@@ -22,4 +22,16 @@ describe("Analytics", () => {
   test("should get accurate number of stores purchased from", () => {
     expect(Analytics.getNumberOfStoresPurchasedFrom()).toEqual(9);
   });
+
+  test("should get accurate total spent by day of week", () => {
+    expect(Analytics.getTotalSpendByDayOfWeek()["Fri"]).toEqual(450);
+  });
+
+  test("should get accurate total spent by month", () => {
+    expect(Analytics.getTotalSpendByMonth()["Jan"]).toEqual(450);
+  });
+
+  test("should get accurate totals for each year", () => {
+      expect(Analytics.getTotalSpendByYear()[2020]).toEqual(450);
+  });
 });
