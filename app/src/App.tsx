e47d5cbab49eb131b6091cbdfca1c0e2662221da
theodoremoreland <theodoremoreland@yahoo.com>
2024-06-07 21:49:58 -0500

    updated to scaffolding

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,7 +6,6 @@ const App = (): ReactElement => {
 
   return (
     <>
-
     </>
   )
 }
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Components
 import App from './App.tsx';
+
+// Styles
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
