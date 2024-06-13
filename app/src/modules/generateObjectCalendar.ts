359aa4a196c6f157851d661da52fff86a412c6c2
theodoremoreland <theodoremoreland@yahoo.com>
2024-06-12 23:53:59 -0500

    added function for generating an object calendar

---
+++ app/src/modules/generateObjectCalendar.ts
@@ -0,0 +1,24 @@
+import { DateFormat } from "../types/types"
+
+/**
+ * Create an array of objects with a date property.
+ * This can be used to create a calendar of orders wherein
+ * some dates may not have orders.
+ */
+export default (startDate: Date, endDate: Date): { date: DateFormat }[] => {  
+    const data: { date: DateFormat }[] = [];
+
+    for (let d = startDate; d <= endDate; d.setDate(d.getDate() + 1)) {
+        const dateUnits: string[] = d.toString().split(' ');
+        const month: string = dateUnits[1];
+        const day: string = dateUnits[2];
+        const year: string = dateUnits[3];
+        const formattedDate: DateFormat = `${month} ${day} ${year}` as DateFormat;
+
+        data.push({
+            date: formattedDate,
+        });
+    }
+
+    return data;
+}
