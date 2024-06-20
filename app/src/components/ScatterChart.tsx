82ebd7b36cec46daa6f3928751ec7b799e9f631d
theodoremoreland <theodoremoreland@yahoo.com>
2024-06-19 20:55:09 -0500

    updated format of scatter plot

--- app/src/components/ScatterChart.tsx
+++ app/src/components/ScatterChart.tsx
@@ -13,12 +13,27 @@ const Scatter = ({ data }: Props): ReactElement => {
     return (
         <div>
             <ScatterChart
+                grid={{
+                    horizontal: true
+                }}
                 series={[
                     {
-                        data: data
+                        data,
+                        valueFormatter: ({ x, y }) => `${y.toLocaleString('en-US', { style: 'currency', currency: 'USD' })} (${new Date(x).toLocaleDateString()})`,
                     }
                 ]}
-                width={1280}
+                yAxis={[
+                    {
+                        min: 1,
+                        valueFormatter: (value) => value.toLocaleString('en-US', { style: 'currency', currency: 'USD' }),
+                    }
+                ]}
+                xAxis={[
+                    {
+                        valueFormatter: (value) => new Date(value).toLocaleDateString()
+                    }
+                ]}
+                width={1400}
                 height={300}
             />
         </div>
