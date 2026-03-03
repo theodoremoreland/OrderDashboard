24e14ff9ddd1538928d5b48902117cf6e25783fe
Theodore Moreland <theodoremoreland@yahoo.com>
2026-03-02 23:29:49 -0600

    added trophy icons to Top Streaks + slightly adjusting spacing

--- app/src/components/List.tsx
+++ app/src/components/List.tsx
@@ -1,34 +1,59 @@
 // React
-import { ReactElement } from "react";
+import { ReactElement } from 'react';
+
+// Images
+import TrophyIcon from '../assets/images/icons/trophy.svg?react';
 
 // Styles
 import './List.css';
 
 interface Props {
-    id: string
-    title: string
+    id: string;
+    title: string;
     data: {
-        key: string | number,
-        value: string | number
-    }[]
+        key: string | number;
+        value: string | number;
+    }[];
 }
+
+const displayTrophyIcon = (index: number): ReactElement | null => {
+    if (index === 0) {
+        return <TrophyIcon className="trophy-icon gold" />;
+    } else if (index === 1) {
+        return <TrophyIcon className="trophy-icon silver" />;
+    } else if (index === 2) {
+        return <TrophyIcon className="trophy-icon bronze" />;
+    } else {
+        // Return an invisible trophy icon to maintain spacing for items without a trophy.
+        return <TrophyIcon className="trophy-icon invisible" />;
+    }
+};
 
 const List = ({ id, title, data }: Props): ReactElement => {
     return (
-        <ul
-            id={id}
-            data-testid={id}
-            title={title}
-            className="List"
-        >
-            {data.map(({ key, value } : { key: string | number, value: string | number }) => (
-                <li key={`${key} ${value}`}>
-                    <span className="key">{key}</span>
-                    <span className="value">{value}</span>
-                </li>
-            ))}
+        <ul id={id} data-testid={id} title={title} className="List">
+            {data.map(
+                (
+                    {
+                        key,
+                        value,
+                    }: {
+                        key: string | number;
+                        value: string | number;
+                    },
+                    index
+                ) => (
+                    <li key={`${key} ${value}`}>
+                        <span className="key">
+                            {displayTrophyIcon(index)}
+                            <p>{key}</p>
+                        </span>
+                        <span className="value">{value}</span>
+                    </li>
+                )
+            )}
         </ul>
-    )
-}
+    );
+};
 
 export default List;
--- app/src/components/TopStreaks/TopStreaks.tsx
+++ app/src/components/TopStreaks/TopStreaks.tsx
@@ -1,5 +1,5 @@
 // React
-import { ReactElement, useState, useContext } from "react";
+import { ReactElement, useState, useContext } from 'react';
 
 // MUI
 import FormControl from '@mui/material/FormControl';
@@ -7,30 +7,32 @@ import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 
 // Custom
-import Analytics from "../../classes/Analytics";
+import Analytics from '../../classes/Analytics';
 
 // Context
-import { DisplaySettingsContext } from "../../contexts/DisplaySettingsProvider";
+import { DisplaySettingsContext } from '../../contexts/DisplaySettingsProvider';
 
 // Components
-import List from "../List";
+import List from '../List';
 
 // Styles
 import './TopStreaks.css';
 
 interface Props {
-    analytics: Analytics
-    startDate: Date
-    endDate: Date
+    analytics: Analytics;
+    startDate: Date;
+    endDate: Date;
 }
 
 const TopStreaks = ({ analytics, startDate, endDate }: Props): ReactElement => {
     const { listsCount } = useContext(DisplaySettingsContext);
-    const [listSelection, setListSelection] = useState<"days-with-purchases" | "days-without-purchases">("days-with-purchases");
+    const [listSelection, setListSelection] = useState<
+        'days-with-purchases' | 'days-without-purchases'
+    >('days-with-purchases');
 
     return (
         <section className="TopStreaks">
-            <div className='heading'>
+            <div className="heading">
                 <h2>Top Streaks</h2>
                 <FormControl
                     variant="standard"
@@ -40,44 +42,58 @@ const TopStreaks = ({ analytics, startDate, endDate }: Props): ReactElement => {
                     <Select
                         sx={{ color: '#feffff' }}
                         value={listSelection}
-                        onChange={(e) => setListSelection(e.target.value as "days-with-purchases" | "days-without-purchases")}
+                        onChange={(e) =>
+                            setListSelection(
+                                e.target.value as
+                                    | 'days-with-purchases'
+                                    | 'days-without-purchases'
+                            )
+                        }
                         inputProps={{
                             name: 'Top Streaks',
                             id: 'uncontrolled-native',
                         }}
                     >
                         <MenuItem value="days-with-purchases">Orders</MenuItem>
-                        <MenuItem value="days-without-purchases">No Orders</MenuItem>
+                        <MenuItem value="days-without-purchases">
+                            No Orders
+                        </MenuItem>
                     </Select>
                 </FormControl>
             </div>
-            {
-                listSelection === "days-with-purchases" &&
+            {listSelection === 'days-with-purchases' && (
                 <List
                     id="days-with-purchases"
                     title="Longest consecutive days of purchases"
                     key={`${startDate} - ${endDate}`}
                     data={analytics
                         .getTopPurchaseStreaks(startDate, endDate, listsCount)
-                        .map(obj => ({ key: `${obj.days} days`, value: `${new Date(obj.startDate).toLocaleDateString()} - ${new Date(obj.endDate).toLocaleDateString()}` }))
-                    }
+                        .map((obj) => ({
+                            key: `${obj.days} days`,
+                            value: `${new Date(obj.startDate).toLocaleDateString()} - ${new Date(obj.endDate).toLocaleDateString()}`,
+                        }))}
                 />
-            }
-            {
-                listSelection === "days-without-purchases" &&
+            )}
+            {listSelection === 'days-without-purchases' && (
                 <List
                     id="days-without-purchases"
                     title="Longest consecutive days without purchases"
                     key={`${startDate} - ${endDate}`}
                     data={analytics
-                        .getTopDroughtsBetweenPurchases(startDate, endDate, listsCount)
-                        .map(obj => ({ key: `${obj.days} days`, value: `${new Date(obj.startDate).toLocaleDateString()} - ${new Date(obj.endDate).toLocaleDateString()}` }))
-                    }
+                        .getTopDroughtsBetweenPurchases(
+                            startDate,
+                            endDate,
+                            listsCount
+                        )
+                        .map((obj) => ({
+                            key: `${obj.days} days`,
+                            value: `${new Date(obj.startDate).toLocaleDateString()} - ${new Date(obj.endDate).toLocaleDateString()}`,
+                        }))}
                 />
-            }
+            )}
             <div className="overlay"></div>
         </section>
     );
-}
+};
 
 export default TopStreaks;
